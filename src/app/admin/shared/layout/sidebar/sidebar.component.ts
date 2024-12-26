e71ecc08bf1fdcab42c06be20d54322de308e30f
Hamed Naghdi <hamed.naghdi@gmail.com>
2024-12-26 11:08:19 +0100

    complete version

--- src/app/admin/shared/layout/sidebar/sidebar.component.ts
+++ src/app/admin/shared/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {LogoComponent} from '../logo/logo.component';
 import {MenuComponent} from '../menu/menu.component';
+import {VersionComponent} from '../version/version.component';
 
 @Component({
   selector: 'hami-sidebar',
   imports: [
     LogoComponent,
-    MenuComponent
+    MenuComponent,
+    VersionComponent
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
---
+++ src/app/admin/shared/layout/version/version.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hami-version',
+  imports: [],
+  templateUrl: './version.component.html',
+  styleUrl: './version.component.scss'
+})
+export class VersionComponent {
+
+}
